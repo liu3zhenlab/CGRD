d4e554d4a5cd424bbd0129c62e9e2a21922e0d62
liu3zhenlab <liu3zhen@ksu.edu>
2020-09-11 00:29:35 -0500

    demo

--- demo/reads/1c-simulated.sh
+++ demo/reads/1c-simulated.sh
@@ -1,3 +1,3 @@
-dwgsim -e 0 -E 0 -1 100 -2 100 -N 6000 -r 0 -F 0 -R 0 ../genome/ref.fasta ref
-dwgsim -e 0 -E 0 -1 100 -2 100 -N 6000 -r 0 -F 0 -R 0 ../genome/qry.fasta qry
+dwgsim -e 0 -E 0 -1 100 -2 100 -N 12000 -r 0 -F 0 -R 0 ../genome/ref.fasta ref
+dwgsim -e 0 -E 0 -1 100 -2 100 -N 12000 -r 0 -F 0 -R 0 ../genome/qry.fasta qry
 
