46ee74319883894caf3501bc21bd4145392a0312
Sanzhen Liu <liu3zhen@ksu.edu>
2020-09-11 14:07:14 -0500

    Update cgrd.run.sh

--- demo/cgrd.run.sh
+++ demo/cgrd.run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-ref=../genome/ref.fasta
-reffq1=../reads/ref.1.fastq
-reffq2=../reads/ref.2.fastq
-qryfq1=../reads/qry.1.fastq
-qryfq2=../reads/qry.2.fastq
+ref=./genome/ref.fasta
+reffq1=./reads/ref.1.fastq
+reffq2=./reads/ref.2.fastq
+qryfq1=./reads/qry.1.fastq
+qryfq2=./reads/qry.2.fastq
 
 perl cgrd --ref $ref \
 	--subj ref --sfq1 $reffq1 --sfq2 $reffq2 \
